e310a6e52a0d98339107d457cf49b3e6a78a6b6a
binaryCookies <csr@yanalkamal.com>
2024-05-28 15:48:24 -0400

    Final/challenge/11+12/both completed

--- force-app/main/default/lwc/boatDetailTabs/boatDetailTabs.js
+++ force-app/main/default/lwc/boatDetailTabs/boatDetailTabs.js
@@ -18,9 +18,21 @@ import labelPleaseSelectABoat from "@salesforce/label/c.Please_select_a_boat";
 import BOAT_ID_FIELD from "@salesforce/schema/Boat__c.Id";
 import BOAT_NAME_FIELD from "@salesforce/schema/Boat__c.Name";
 
-const BOAT_FIELDS = [BOAT_ID_FIELD, BOAT_NAME_FIELD];
+import BOAT_DESCRIPTION_FIELD from "@salesforce/schema/Boat__c.Description__c";
+import BOAT_TYPE_FIELD from "@salesforce/schema/Boat__c.BoatType__c";
+import BOAT_PRICE_FIELD from "@salesforce/schema/Boat__c.Price__c";
+import BOAT_LENGTH_FIELD from "@salesforce/schema/Boat__c.Length__c";
 
-import { getRecord } from "lightning/uiRecordApi";
+const BOAT_FIELDS = [
+  BOAT_ID_FIELD,
+  BOAT_NAME_FIELD,
+  BOAT_DESCRIPTION_FIELD,
+  BOAT_TYPE_FIELD,
+  BOAT_PRICE_FIELD,
+  BOAT_LENGTH_FIELD
+];
+
+import { getRecord, getFieldValue } from "lightning/uiRecordApi";
 
 import {
   MessageContext,
@@ -31,10 +43,11 @@ import {
 import BOATMC from "@salesforce/messageChannel/BoatMessageChannel__c";
 import { NavigationMixin } from "lightning/navigation";
 
-export default class BoatDetailTabs extends LightningElement {
+export default class BoatDetailTabs extends NavigationMixin(LightningElement) {
   @api boatId; // set by parent component
-
+  @api boat;
   wiredRecord;
+
   label = {
     labelDetails,
     labelReviews,
@@ -44,23 +57,34 @@ export default class BoatDetailTabs extends LightningElement {
   };
 
   @wire(MessageContext) messageContext;
-
+  // Sounde, a02aj000001UFR4AAO
+  // @wire(getRecord, { recordId: "$boatId", fields: BOAT_FIELDS })
   @wire(getRecord, { recordId: "$boatId", fields: BOAT_FIELDS })
-  wiredBoat({ error, data }) {
+  wiredBoat(response) {
+    const { data, error } = response;
     if (data) {
+      console.log("boatDetailsTab.jswiredBoat data:", data);
       this.wiredRecord = data;
       this.error = undefined;
     } else if (error) {
+      console.log("boatDetailsTab.jswiredBoat error:", error);
       this.error = error;
       this.wiredRecord = undefined;
     }
   }
   // Decide when to show or hide the icon
   // returns 'utility:anchor' or null
-  get detailsTabIconName() {}
+  get detailsTabIconName() {
+    console.log("detailsTabIconName called");
+    return this.wiredRecord ? "utility:anchor" : null;
+  }
 
   // Utilize getFieldValue to extract the boat name from the record wire
-  get boatName() {}
+  get boatName() {
+    const fieldName = getFieldValue(this.wiredRecord, BOAT_NAME_FIELD);
+    console.log("boatName called fieldName:", fieldName);
+    return fieldName;
+  }
 
   // Private
   subscription = null;
@@ -72,7 +96,6 @@ export default class BoatDetailTabs extends LightningElement {
 
   disconnectedCallback() {
     this.unsubscribeMC();
-    this.subscription = null;
   }
 
   // Subscribe to the message channel
@@ -82,15 +105,23 @@ export default class BoatDetailTabs extends LightningElement {
         this.messageContext,
         BOATMC,
         (message) => {
+          console.log("boatDetailsTab.js Message received:", message);
+
           this.boatId = message.recordId;
+          this.boat = message.boatData;
         },
         { scope: APPLICATION_SCOPE }
       );
     }
   }
 
+  unsubscribeMC() {
+    unsubscribe(this.subscription);
+    this.subscription = null;
+  }
   // Navigates to record page
   navigateToRecordViewPage() {
+    console.log("navigateToRecordViewPage called");
     this[NavigationMixin.Navigate]({
       type: "standard__objectPage",
       attributes: {
